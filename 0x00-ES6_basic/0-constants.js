da072bd582ebc701d3902c80cd88de8c4d4a5fdf
Abdelwahab-Hamada <abdelwahab.hamada@gmail.com>
2024-03-04 11:52:33 +0000

    0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,6 +3,11 @@ export function taskFirst() {
   return task;
 }
 
+export function getLast() {
+  return ' is okay';
+}
+
+
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
